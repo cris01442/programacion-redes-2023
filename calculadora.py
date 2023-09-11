4fecc34f67562b0b2bb501d7052c773a805fb7b6
cris01442 <azzermin@gmail.com>
2023-09-11 12:36:06 -0500

    o_o

---
+++ calculadora.py
@@ -0,0 +1,16 @@
+'''
+Autor: Cristopher López 
+Fecha: 11 sep 23
+Descripción: calculadora que suma
+'''
+
+a = input("Ingresa el primer valor ")
+b = input("Ingresa el segundo vlaor ")
+print(type(a), type(b)) 
+print ("suma ", a + b)
+a = int(a)
+b = int(b)
+print(type(a), type(b)) 
+print ("suma ", a + b)
+
+ 
