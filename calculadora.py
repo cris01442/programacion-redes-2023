a1ada4198914cedfb1cbb728c0e04b59bb5a9aca
cris01442 <azzermin@gmail.com>
2023-09-11 12:51:01 -0500

    o-o

--- calculadora.py
+++ calculadora.py
@@ -11,6 +11,17 @@ print ("suma ", a + b)
 a = int(a)
 b = int(b)
 print(type(a), type(b)) 
+print("suma de valores")
 print ("suma ", a + b)
 
- 
+print("resta de valores")
+print ("resta ", a - b)
+
+print("division de valores")
+print ("division ", a / b)
+
+print("multiplicacion de valores")
+print ("multiplicacion ", a * b)
+
+
+
