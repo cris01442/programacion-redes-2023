08ba374c0b15c17a96243635d0df4825b1d8a047
cris01442 <144488374+cris01442@users.noreply.github.com>
2023-11-17 13:52:48 -0600

    Add files via upload

---
+++ unidad2_r1/prueba.py
@@ -0,0 +1,45 @@
+'''
+Cristopher López Jiménez 
+GIR0541
+'''
+import http.client
+
+while True:
+    
+    api_key = input("Ingresa tu clave de la API (o 'exit' para salir): ").strip()
+
+    
+    if api_key.lower() == 'exit':
+        break
+
+   
+    if not api_key:
+        print("Error: Debes ingresar una clave de API.")
+        continue
+
+    conn = http.client.HTTPSConnection("jokes-by-api-ninjas.p.rapidapi.com")
+
+    headers = {
+        'X-RapidAPI-Key': api_key,
+        'X-RapidAPI-Host': "jokes-by-api-ninjas.p.rapidapi.com"
+    }
+
+    conn.request("GET", "/v1/jokes", headers=headers)
+
+    res = conn.getresponse()
+    data = res.read()
+
+   
+    if res.status == 200:
+        print("Respuesta de la API:")
+        print(data.decode("utf-8"))
+    else:
+        print(f"Error en la solicitud. Código de estado: {res.status}")
+
+    conn.close()
+
+# Fin del programa
+print("¡Hasta luego!")
+
+
+
