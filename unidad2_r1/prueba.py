59df66bc547cd052c714331c298fcfd1dbdaf91e
cris01442 <144488374+cris01442@users.noreply.github.com>
2023-11-17 13:56:04 -0600

    Update prueba.py

--- unidad2_r1/prueba.py
+++ unidad2_r1/prueba.py
@@ -1,6 +1,7 @@
 '''
 Cristopher López Jiménez 
 GIR0541
+key: c470b1851cmshed028756f3fdb29p16695ejsn3b18a2974294
 '''
 import http.client
 
@@ -8,11 +9,9 @@ while True:
     
     api_key = input("Ingresa tu clave de la API (o 'exit' para salir): ").strip()
 
-    
     if api_key.lower() == 'exit':
         break
 
-   
     if not api_key:
         print("Error: Debes ingresar una clave de API.")
         continue
@@ -29,7 +28,6 @@ while True:
     res = conn.getresponse()
     data = res.read()
 
-   
     if res.status == 200:
         print("Respuesta de la API:")
         print(data.decode("utf-8"))
@@ -38,8 +36,6 @@ while True:
 
     conn.close()
 
-# Fin del programa
 print("¡Hasta luego!")
-
 
 
