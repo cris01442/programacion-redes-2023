fcc4268c46730767b24ab06d3527b660693fe282
cris01442 <144488374+cris01442@users.noreply.github.com>
2023-11-27 17:12:36 -0600

    Update prueba.py

--- unidad2_r1/prueba.py
+++ unidad2_r1/prueba.py
@@ -5,17 +5,9 @@ key: c470b1851cmshed028756f3fdb29p16695ejsn3b18a2974294
 '''
 import http.client
 
+api_key = "c470b1851cmshed028756f3fdb29p16695ejsn3b18a2974294"
+
 while True:
-    
-    api_key = input("Ingresa tu clave de la API (o 'exit' para salir): ").strip()
-
-    if api_key.lower() == 'exit':
-        break
-
-    if not api_key:
-        print("Error: Debes ingresar una clave de API.")
-        continue
-
     conn = http.client.HTTPSConnection("jokes-by-api-ninjas.p.rapidapi.com")
 
     headers = {
@@ -36,6 +28,12 @@ while True:
 
     conn.close()
 
+    exit_input = input("¿Quieres salir? Ingresa 'exit' para salir, o presiona Enter para continuar: ").strip().lower()
+    if exit_input == 'exit':
+        break
+
 print("¡Hasta luego!")
+
+    
 
 
